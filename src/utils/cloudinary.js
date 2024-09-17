cc0018ac5b73f509b68f43a3b10786d89b2ae8bb
Praanshu Grover <praanshugrover60@gmail.com>
2024-09-17 05:46:54 +0530

    adds delete functionality for cloudinary images with enhanced error handling in upload and delete operations

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,31 +1,51 @@
-import {v2 as cloudinary} from "cloudinary"
-import fs from "fs"
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+import { ApiError } from "./ApiError.js";
 
-
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
-    try {
-        if (!localFilePath) return null
-        //upload the file on cloudinary
-        const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type: "auto"
-        })
-        // file has been uploaded successfull
-        //console.log("file is uploaded on cloudinary ", response.url);
-        fs.unlinkSync(localFilePath)
-        return response;
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfully
+    //console.log("file is uploaded on cloudinary ", response.url);
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    return null;
+  }
+};
 
-    } catch (error) {
-        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
-        return null;
-    }
-}
+const deleteOnCloudinary = async (cloudinaryUrl, resource_type = "image") => {
+  try {
+    if (!cloudinaryUrl) return new ApiError(400, "Cloudinary url is required");
 
+    const publicId = cloudinaryUrl.split("/").pop().split(".")[0];
+    console.log(publicId);
 
+    // delete the file on cloudinary
+    const response = await cloudinary.uploader.destroy(publicId, {
+      resource_type,
+    });
+    console.log(response);
 
-export {uploadOnCloudinary}
+    // file has been deleted successfully
+    return response;
+  } catch (error) {
+    return new ApiError(
+      500,
+      error || "Something went wrong while deleting file"
+    );
+  }
+};
+
+export { uploadOnCloudinary, deleteOnCloudinary };
