fa2f45d428d85eccaa379124d256f30d97dfae2f
Praanshu Grover <praanshugrover60@gmail.com>
2024-09-20 07:00:01 +0530

    feat: implement updateTweet function with validation checks and success response in tweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -53,6 +53,49 @@ const getUserTweets = asyncHandler(async (req, res) => {
 
 const updateTweet = asyncHandler(async (req, res) => {
   //TODO: update tweet
+
+  const { tweetId } = req.params;
+
+  // Check if tweetId is valid
+  if (!isValidObjectId(tweetId)) {
+    throw new ApiError(400, "Invalid tweet id");
+  }
+
+  // Get tweet details
+  const tweet = await Tweet.findById(tweetId);
+  if (!tweet) {
+    throw new ApiError(404, "Tweet does not exist");
+  }
+
+  // Check if user is owner of the tweet
+  if (String(req.user._id) !== String(tweet.owner)) {
+    throw new ApiError(401, "Unauthorized request");
+  }
+
+  const { content } = req.body;
+
+  // Check if content is empty
+  if (!content?.trim()) {
+    throw new ApiError(400, "Content is required");
+  }
+
+  // Update tweet
+  const updatedTweet = await Tweet.findOneAndUpdate(
+    new mongoose.Types.ObjectId(tweetId),
+    {
+      $set: {
+        content,
+      },
+    }
+  );
+
+  if (!updatedTweet) {
+    throw new ApiError(500, "Something went wrong while updating tweet");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedTweet, "Tweet updated successfully"));
 });
 
 const deleteTweet = asyncHandler(async (req, res) => {
