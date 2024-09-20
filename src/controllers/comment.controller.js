ea7014df31db021054c050a796e3147404b28a98
Praanshu Grover <praanshugrover60@gmail.com>
2024-09-21 04:45:08 +0530

    feat: enhance addComment function with validation and error handling for adding comments to a video in comment controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,31 +1,59 @@
-import mongoose from "mongoose"
-import {Comment} from "../models/comment.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { mongoose, isValidObjectId } from "mongoose";
+import { Comment } from "../models/comment.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Video } from "../models/video.model.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
-
-})
+  //TODO: get all comments for a video
+  const { videoId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
+});
 
 const addComment = asyncHandler(async (req, res) => {
-    // TODO: add a comment to a video
-})
+  // TODO: add a comment to a video
+  const { videoId } = req.params;
+  const { content } = req.body;
+
+  // Check if videoId is valid
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video id");
+  }
+
+  // Check if content is empty
+  if (!content?.trim()) {
+    throw new ApiError(400, "Content is required");
+  }
+
+  // Check if video exists
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(404, "Video does not exist");
+  }
+
+  // Create comment
+  const createdComment = await Comment.create({
+    content,
+    video: videoId,
+    owner: req.user._id,
+  });
+
+  if (!createdComment) {
+    throw new ApiError(500, "Something went wrong while creating comment");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, createdComment, "Comment created successfully"));
+});
 
 const updateComment = asyncHandler(async (req, res) => {
-    // TODO: update a comment
-})
+  // TODO: update a comment
+});
 
 const deleteComment = asyncHandler(async (req, res) => {
-    // TODO: delete a comment
-})
+  // TODO: delete a comment
+});
 
-export {
-    getVideoComments, 
-    addComment, 
-    updateComment,
-     deleteComment
-    }
+export { getVideoComments, addComment, updateComment, deleteComment };
