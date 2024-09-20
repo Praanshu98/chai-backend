751200141703f7185c060f7d0b277426e91c6fc9
Praanshu Grover <praanshugrover60@gmail.com>
2024-09-21 05:19:52 +0530

    feat: implement deleteComment function with validation and error handling in comment controller for safer comment deletions

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -90,6 +90,30 @@ const updateComment = asyncHandler(async (req, res) => {
 
 const deleteComment = asyncHandler(async (req, res) => {
   // TODO: delete a comment
+
+  const { commentId } = req.params;
+
+  // Check if commentId is valid
+  if (!isValidObjectId(commentId)) {
+    throw new ApiError(400, "Invalid comment id");
+  }
+
+  // Check if comment exists
+  const comment = await Comment.findById(commentId);
+  if (!comment) {
+    throw new ApiError(404, "Comment does not exist");
+  }
+
+  // Delete comment
+  const deletedComment = await Comment.findByIdAndDelete(commentId);
+
+  if (!deletedComment) {
+    throw new ApiError(500, "Something went wrong while deleting comment");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, deletedComment, "Comment deleted successfully"));
 });
 
 export { getVideoComments, addComment, updateComment, deleteComment };
