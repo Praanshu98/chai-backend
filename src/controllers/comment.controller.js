f96bf08d7c082a37167aea2afd673ba0ef7bbda9
Praanshu Grover <praanshugrover60@gmail.com>
2024-09-21 05:39:28 +0530

    feat: adds getVideoComments endpoint

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -9,6 +9,40 @@ const getVideoComments = asyncHandler(async (req, res) => {
   //TODO: get all comments for a video
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
+
+  // Check if videoId is valid
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video id");
+  }
+
+  // Check if video exists
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(404, "Video does not exist");
+  }
+
+  // Get comments for video
+  const comments = await Comment.aggregate([
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $skip: (Number(page) - 1) * Number(limit),
+    },
+    {
+      $limit: Number(limit),
+    },
+  ]);
+
+  if (!comments) {
+    throw new ApiError(500, "Something went wrong while getting comments");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, comments, "Comments fetched successfully"));
 });
 
 const addComment = asyncHandler(async (req, res) => {
